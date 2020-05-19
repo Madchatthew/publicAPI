4ea160599c8f79fea07901d52683e2b55b44128d
Madchatthew <chad.jessen@gmail.com>
2020-05-19 22:45:14 +0000

    index/show completed

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const express = require('express')
-const app = express()
-const request = require('request')
+const   bodyParser      = require('body-parser'),
+        mongoose        = require('mongoose'),
+        express         = require('express'),
+        request         = require('request'),
+        app             = express()
+
+mongoose.connect('mongodb://localhost/publicAPI', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+const publicSchema = new mongoose.Schema( {
+    title: String,
+    category: String,
+    description: String,
+    link: String,
+    comments: String,
+    created: {type: Date, default: Date.now}
+})
+
+const Public = mongoose.model('Public', publicSchema)
 
 app.get('/', function(req, res) {
     res.render('search')
@@ -16,6 +39,16 @@ app.get('/results', function(req, res) {
             let data = JSON.parse(body)
             res.render('results', {data: data})
             // res.send(results['entries'][0]['API'])
+        }
+    })
+})
+
+app.get('/favorites', function(req, res) {
+    Public.find({}, function(err, favorites) {
+        if(err) {
+            console.log('ERROR!')
+        } else {
+            res.render('favorites', {favorites: favorites})
         }
     })
 })
