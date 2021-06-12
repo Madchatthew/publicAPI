4db0b63c4523f5d155f3514d8d499abce7149ca0
Madchatthew <chad.jessen@gmail.com>
2021-06-12 05:34:03 -0500

    working on getting error message to display on page above search bar

--- routes/results.js
+++ routes/results.js
@@ -2,21 +2,53 @@ const express = require('express');
 const got = require('got');
 const router = express.Router();
 
-// shows the results of search
+
 router.get('/', async (req, res) => {
-        try {
-            let query = req.query.search;
-            let url = process.env.API_URL + query + process.env.API_OPTIONS;
-            const response = await got(url);
-            let data = JSON.parse(response.body);
+    let query = req.query.search;
+    let url = process.env.API_URL + query + process.env.API_OPTIONS;
+    const response = await got(url);
+    let data = JSON.parse(response.body);
+    function resultsPromise() {
+        return new Promise((resolve, reject) => {
             if (data['entries'] === null) {
-                res.redirect('/');
-            } else {
-                res.render('results/index', {data});
-            }
-        } catch (error) {
-            res.json({ message: error.message });
+                // Add variable locals here to see if works
+                reject ({
+                    message: 'No results from search term. Please use another Search term'
+                })
+                } else {
+                    resolve(res.render('results/index', {
+                        data,
+                        message: 'Here are your results'
+                    }));
+                }
+            })
         }
+    resultsPromise().then((message) => {
+        console.log('it worked');
+    }).catch((error) => {
+         res.render('index', {error});
+    })
 })
+
+
+
+// shows the results of search
+// router.get('/', async (req, res) => {
+//         try {
+//             let query = req.query.search;
+//             let url = process.env.API_URL + query + process.env.API_OPTIONS;
+//             const response = await got(url);
+//             let data = JSON.parse(response.body);
+//             if (data['entries'] === null) {
+//                 throw new Error();
+//                 // res.redirect('/');
+//             } else {
+//                 res.render('results/index', {data});
+//             }
+//         } catch (error) {
+//             res.json(error);
+//             // res.json({ message: error.message });
+//         }
+// })
 
 module.exports = router;
